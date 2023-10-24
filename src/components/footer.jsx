225d20c647c003166d8cdcdab8dabb66a2fadcbf
Chatzyr <chatverse90@gmail.com>
2023-10-25 01:50:15 +0500

    link

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './footer.css'
+import { Link } from 'react-router-dom';
 
 
 function Footer() {
@@ -7,9 +8,18 @@ function Footer() {
     <footer className='footerStyle'>
       <div className='footerContentStyle'>
         <ul className='menuStyle' >
-          <li className='menuItemStyle'><a className='linkStyle' href="/termsofuse">Terms of Use </a></li>
-          <li className='menuItemStyle'><a className='linkStyle' href="/contactus">Contact Us</a></li>
-          <li className='menuItemStyle'><a className='linkStyle' href="/privacyterms">Privacy Policy</a></li>
+          <li className='menuItemStyle'>
+            {/* <a className='linkStyle' href="/termsofuse">Terms of Use </a> */}
+            <Link className="linkStyle" to="/termsofuse">Terms of Use</Link>
+          </li>
+          <li className='menuItemStyle'>
+            {/* <a className='linkStyle' href="/contactus">Contact Us</a> */}
+            <Link className="linkStyle" to="/contactus">Contact Us</Link>
+            </li>
+          <li className='menuItemStyle'>
+            {/* <a className='linkStyle' href="/privacyterms">Privacy Policy</a> */}
+            <Link className="linkStyle" to="/privacyterms">Privacy Policy</Link>
+            </li>
         </ul>
         <div className='copyrightStyle'>
           <span className='chatzyrStyle'>Chatzyr</span> Copyright &copy; {new Date().getFullYear()}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import logo from './weblogo.png';
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -14,7 +15,8 @@ function Navbar() {
       <nav className="navbar">
         <div className="logo-container">
           <img src={logo} alt="Logo" className="logo1" />
-          <a className="menu-link2" href="/">Chatzyr</a>
+          {/* <a className="menu-link2" href="/">Chatzyr</a> */}
+          <Link className="menu-link2" to="/">Chatzyr</Link>
         </div>
         <div className="menu-toggle" onClick={toggleMenu}>
           <div className={`hamburger ${isMenuOpen ? 'open' : ''}`}>
@@ -25,16 +27,20 @@ function Navbar() {
         </div>
         <ul className={`menu ${isMenuOpen ? 'active' : ''}`}>
           <li className="menu-item">
-            <a className="menu-link" href="/">Home</a>
+            {/* <a className="menu-link" href="/">Home</a> */}
+            <Link className="menu-link" to="/">Home</Link>
           </li>
           <li className="menu-item">
-            <a className="menu-link" href="/">Shop</a>
+            {/* <a className="menu-link" href="/">Shop</a> */}
+            <Link className="menu-link" to="/">Shop</Link>
           </li>
           <li className="menu-item">
-            <a className="menu-link" href="/report">Report</a>
+            <Link className="menu-link" to="/report">Report</Link>
+            {/* <Link></Link> */}
           </li>
           <li className="menu-item">
-            <a className="menu-link" href="/suggestion">Suggest</a>
+            {/* <a className="menu-link" href="/suggestion">Suggest</a> */}
+            <Link className="menu-link" to="/suggestion">Suggest</Link>
           </li>
         </ul>
       </nav>
