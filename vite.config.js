bffbc14054c4c573d3fcd004de5a44cbb7f1fbd3
chatverse90 <142596036+chatverse90@users.noreply.github.com>
2024-01-08 16:32:27 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import reactLoading from 'react-loading';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-loading'],
-    },
-  },
 })
